2afddd7fcf248afa35186918d85780b8938a87ff
zack-guo <52080300+zack-guo@users.noreply.github.com>
2020-10-27 23:52:53 +0800

    cgi-base
    low-func version

--- server.py
+++ server.py
@@ -1,35 +1,16 @@
-import socketserver
+from http.server import HTTPServer, CGIHTTPRequestHandler
 
-port = ('127.0.0.1', 8000)
+if __name__ == '__main__':
+    try:
+        CGIHTTPRequestHandler.cgi_directories = ['/cgi-bin']
 
-class fServer(socketserver.BaseRequestHandler):
-    def handle(self):
-        print("Connection:  ", self.request)
-        print("Address: ", self.client_address)
+        httpd = HTTPServer(('', 8010),             # localhost:8000
+                           CGIHTTPRequestHandler)  # CGI support.
 
-        msg = self.request.recv(4096).decode()
+        print(f"Running server. Use [ctrl]-c to terminate.")
 
-        httpHead = msg.split('\r\n')[:-1]   # 去除最后一部分HTTP请求正文，保留请求头部，并按行分割
-        # 首行
-        firstLine = httpHead[0].split(' ')  # 按空格分割首行基本信息
-        method = firstLine[0]
-        url = firstLine[1]
-        version = firstLine[2]
+        httpd.serve_forever()
 
-        labels = []
-        for label in httpHead[1:]:
-            labels.append(label.split(': '))
-
-        # 简单HTTP示例
-        echoBody = """Hi, It's {}\nYou are {}""".format(url, self.client_address).encode()
-        echoHead = """HTTP/1.0 200 OK\r\nContent-Length: {}\r\nServer: Fever's\r\nContent-Type: text/html""".format(len(echoBody)).encode()
-        echoAll = echoHead + b'\r\n\r\n' + echoBody + b'\r\n'
-
-        # 发回response
-        self.request.sendall(echoAll)
-
-
-if __name__ == "__main__":
-    ss = socketserver.ThreadingTCPServer(port, fServer)
-    ss.serve_forever()
-
+    except KeyboardInterrupt:
+        print(f"\nReceived keyboard interrupt. Shutting down server.")
+        httpd.socket.close()
