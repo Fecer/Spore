287df497b66af6dc148c98091cb425d45c1f95d8
KennyH33 <59190591+KennyH33@users.noreply.github.com>
2020-10-28 10:47:52 +0800

    Add files via upload
    可以在python服务器端查看信息，也可以在log文件夹中的log.txt查看log日志。

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socketserver
-
+import logging
+import os
 port = ('127.0.0.1', 8000)
 
 class fServer(socketserver.BaseRequestHandler):
@@ -15,11 +16,20 @@ class fServer(socketserver.BaseRequestHandler):
         method = firstLine[0]
         url = firstLine[1]
         version = firstLine[2]
-
+        src=self.client_address[0]
         labels = []
+        current_path = os.path.dirname(__file__)
+        file = open(current_path + "/log/log.txt", 'w+')
+        file.write('recv http request to {} from {}    method:{} version:{}\n'.format(
+            url,src,method,version))
+        logging.info('recv http request to {} from {}    method:{} version:{}'.format(
+            url,src,method,version))
+        logging.info('labels:')
+        file.write('labels:\n')
         for label in httpHead[1:]:
             labels.append(label.split(': '))
-
+            logging.info('  {}'.format(labels))
+            file.write('  {}\n'.format(labels))
         # 简单HTTP示例
         echoBody = """Hi, It's {}\nYou are {}""".format(url, self.client_address).encode()
         echoHead = """HTTP/1.0 200 OK\r\nContent-Length: {}\r\nServer: Fever's\r\nContent-Type: text/html""".format(len(echoBody)).encode()
@@ -30,6 +40,7 @@ class fServer(socketserver.BaseRequestHandler):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s: %(message)s', level=logging.INFO)
     ss = socketserver.ThreadingTCPServer(port, fServer)
     ss.serve_forever()
 
